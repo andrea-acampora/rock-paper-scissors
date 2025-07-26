87539dd8985792c30bc8e79469aced822e551883
Andrea Acampora <andrea.acampora@protonmail.com>
2025-07-26 16:31:32 +0200

    chore: fix playwright root dir

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,13 +3,13 @@ import { defineConfig } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests/e2e",
   use: {
-    baseURL: "http://localhost:4173/rock-paper-scissors/",
+    baseURL: "http://localhost:5173",
     headless: true,
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
   },
   webServer: {
-    command: "npm run preview",
+    command: "npm run dev",
     timeout: 10000,
     reuseExistingServer: !process.env.CI,
   },
